061bcbf27b5a8443aded452a24ae40cf714411fa
combat47 <115223808+combat47@users.noreply.github.com>
2026-07-10 15:49:04 +0330

    Improve global exeption handling and logging

--- src/main/java/com/amir/taskmanager/exception/ErrorResponse.java
+++ src/main/java/com/amir/taskmanager/exception/ErrorResponse.java
@@ -1,7 +1,27 @@
 package com.amir.taskmanager.exception;
 
-public record ErrorResponse(
-        int status,
-        String message
-) {
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+
+    private int status;
+
+    private String error;
+
+    private String message;
+
+    private String path;
+
+    public ErrorResponse(int status,
+                         String error,
+                         String message,
+                         String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
 }
--- src/main/java/com/amir/taskmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/amir/taskmanager/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.amir.taskmanager.exception;
 
 
+import com.amir.taskmanager.service.TaskService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,15 +14,19 @@ import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(TaskNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleTaskNotFound(
-            TaskNotFoundException ex
+            TaskNotFoundException ex,
+            HttpServletRequest request
     ) {
         return new ErrorResponse(
                 404,
-                ex.getMessage()
+                "Not Found",
+                ex.getMessage(),
+                request.getRequestURI()
         );
     }
 
@@ -38,6 +46,26 @@ public class GlobalExceptionHandler {
                 400,
                 "Validation failed",
                 errors
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        logger.error(
+                "Unhandled exception at {}",
+                request.getRequestURI(),
+                ex
+        );
+
+        return new ErrorResponse(
+                500,
+                "Internal Server Error",
+                "An unexpected error occurred",
+                request.getRequestURI()
         );
     }
 }
