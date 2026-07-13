7fc226052d57973b6b60fe6678e2b58feb7a2823
combat47 <115223808+combat47@users.noreply.github.com>
2026-07-14 00:56:48 +0330

    feat: containerize application with Docker Compose

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
-FROM eclipse-temurin:21-jdk
+FROM maven:3.9.9-eclipse-temurin-21 AS builder
 
 WORKDIR /app
 
-COPY target/task-management-system-1.0-SNAPSHOT.jar app.jar
+COPY pom.xml .
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:21-jre
+
+WORKDIR /app
+
+COPY --from=builder /app/target/*.jar app.jar
 
 EXPOSE 8080
 
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(150) NOT NULL,
-    role VARCHAR(50) NOT NULL
+                       id BIGSERIAL PRIMARY KEY,
+                       username VARCHAR(100) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE projects (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+CREATE TABLE project (
+                          id BIGSERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE tasks (
-    id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    status VARCHAR(50),
-    project_id BIGINT,
-    CONSTRAINT fk_project
-                   FOREIGN KEY(project_id)
-                   REFERENCES projects(id)
-                   ON DELETE CASCADE
+CREATE TABLE task (
+                       id BIGSERIAL PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       status VARCHAR(50),
+                       project_id BIGINT,
+                       CONSTRAINT fk_project
+                           FOREIGN KEY(project_id)
+                               REFERENCES project(id)
+                               ON DELETE CASCADE
 );
