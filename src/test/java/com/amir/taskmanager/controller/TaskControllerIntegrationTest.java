70571bbbf1a58b502c5c3ecfbd73b78be629493d
combat47 <115223808+combat47@users.noreply.github.com>
2026-07-14 13:25:34 +0330

    Update: update TaskControllerIntegrationTest.java and add test user called amir1

--- src/test/java/com/amir/taskmanager/controller/TaskControllerIntegrationTest.java
+++ src/test/java/com/amir/taskmanager/controller/TaskControllerIntegrationTest.java
@@ -2,13 +2,18 @@ package com.amir.taskmanager.controller;
 
 
 import com.amir.taskmanager.dto.CreateTaskRequest;
+import com.amir.taskmanager.model.Role;
+import com.amir.taskmanager.model.User;
+import com.amir.taskmanager.repository.UserRepository;
 import com.amir.taskmanager.service.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +27,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class TaskControllerIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setup() {
+        if (userRepository.findByUsername("amir").isEmpty()) {
+            User user = new User();
+            user.setUsername("amir1");
+            user.setPassword(passwordEncoder.encode("12345678"));
+            user.setRole(Role.ADMIN);
+
+            userRepository.save(user);
+        }
+    }
 
     @Autowired
     private MockMvc mockMvc;
